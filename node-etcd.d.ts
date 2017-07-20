a0b76e1f0ff99efc6ab48890ccde179c590e8606
Derek Brown <derek@allderek.com>
2017-07-20 02:57:41 -0700

    Update node-etcd.d.ts

--- node-etcd.d.ts
+++ node-etcd.d.ts
@@ -1,4 +1,5 @@
 declare module 'node-etcd' {
+   import Etcd = require('node-etcd');
    export class Etcd {
       constructor(hosts?: any, options?: any);
       compareAndDelete(key: any, oldvalue: any, options: any, callback: any): any;
